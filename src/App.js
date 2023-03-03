cefd9a37990049ad3be04e8c9aec47562ef71151
naushinhoque <fnmh@yahoo.com>
2023-03-02 18:28:48 -0600

    needtoaddnames

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Character from './components/Character';
+
+const dummyData = {
+  birth_year: "19BBY",
+  eye_color: "blue",
+  gender: "male",
+  hair_color: "blond",
+  height: "172",
+  mass: "77",
+  name: "Luke Skywalker",
+  skin_color: "fair",
+  }
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [person, setPerson] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        //console.log(res.data);
+        res.data.foreach(() => setPerson(res.data));
+      })
+      .catch(err => console.error(err))
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      { person && <Character attr={person} /> }
     </div>
   );
 }
 
 export default App;
+
+/**
+ 
+birth_year: "19BBY"
+eye_color: "blue"
+gender: "male"
+hair_color: "blond"
+height: "172"
+homeworld: "http://swapi.dev/api/planets/1/"
+mass: "77"
+name: "Luke Skywalker"
+skin_color: "fair"
+
+ */
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+import React from 'react';
+
+const dummyData = {
+    birth_year: "19BBY",
+    eye_color: "blue",
+    gender: "male",
+    hair_color: "blond",
+    height: "172",
+    mass: "77",
+    name: "Luke Skywalker",
+    skin_color: "fair",
+    }
+
+const Characters = (props) => {
+    return (
+        <div className="character-wrapper">
+            <h3>{props.attr.name}</h3>
+            <p>{props.attr.gender}</p>
+            <p>{props.attr.birth_year}</p>
+            <p>{props.attr.eye_color}</p>
+            <p>{props.attr.hair_color}</p>
+            <p>{props.attr.height}</p>
+            <p>{props.attr.mass}</p>
+            <p>{props.attr.skin_color}</p>
+        </div>
+    )
+}
+
+export default Characters;
